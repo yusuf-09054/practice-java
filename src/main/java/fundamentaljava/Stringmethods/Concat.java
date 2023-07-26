e50b38c55ee6f14f1322c292626e720122a2f041
yusuf-09054 <139753483+yusuf-09054@users.noreply.github.com>
2023-07-26 12:34:44 +0530

    Add files via upload

---
+++ src/main/java/fundamentaljava/Stringmethods/Concat.java
@@ -0,0 +1,13 @@
+package fundamentaljava.Stringmethods;
+
+public class Concat {
+    public static void main(String[] args) {
+        String s="Rolex"+"vs"+"Dilli"+"vs"+"Leo";
+        System.out.println(s);
+        String s1="Yusuf";
+        String ss="sanjai";
+        String ss2="Kumar";
+        System.out.println(ss.concat(ss2));
+        System.out.println(s1.concat(s1));
+    }
+}
