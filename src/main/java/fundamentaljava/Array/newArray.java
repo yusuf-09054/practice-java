71c3c4b7d28794ea30b705bf9407444c963115c0
yusuf-09054 <smohammedyusuf193@gmail.com>
2023-07-26 15:10:11 +0530

    new array folder create

---
+++ src/main/java/fundamentaljava/Array/newArray.java
@@ -0,0 +1,8 @@
+package fundamentaljava.Array;
+
+public class newArray
+{
+    public static void main(String[] args) {
+        System.out.println("welcome to array concept execute array types");
+    }
+}
